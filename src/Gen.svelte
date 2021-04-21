1564cfebca76c4c10d52148d7c992e3c09ccf123
Lucassilv2 <lucas.032@acad.pucrs.br>
2021-04-21 19:28:57 -0300

    Tabela melhor

--- src/Gen.svelte
+++ src/Gen.svelte
@@ -69,7 +69,7 @@
                     </Col>    
                 </Row> 
             </div>
-            <table class='rounded-t-lg m-5 w-5/6 mx-auto bg-gray-200 text-gray-800'>
+            <table class='rounded-t-lg m-5 w-full mx-auto bg-gray-200 text-gray-800'>
                 <tr class='text-left border-b-2 border-gray-300 ' >
                     <th class='px-4 py-3'>População</th>
                     <th class='p-0' width='110px'>Fitnesses</th>
